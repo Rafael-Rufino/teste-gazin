6dff2e2103f411cc006918c647989f3e083b2f30
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 01:57:32 -0300

    feat: definindo a paleta de cores

---
+++ front-end/src/styles/themes.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    primary: "#E51C44",
+    background: "#191929",
+    black: "#0000",
+    heading: "#FFF",
+    green: "#32BD50",
+    label: "#333",
+    text: "#787880",
+    gray: "#787898",
+    secondary: "#ff9000",
+    secondaryHover: "#f1972c",
+  },
+};
+export default theme;
