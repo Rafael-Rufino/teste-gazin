04ab43a590b60b88fb2c092297d545cc6d012dd5
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 03:18:30 -0300

    refactor: ajuste no componente de header

--- front-end/src/components/Header/styles.ts
+++ front-end/src/components/Header/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.header`
   padding: 30px 20px;
   justify-content: space-between;
   align-items: center;
-  background: ${theme.colors.black};
+  background: ${theme.colors.background};
   border-bottom: 1px solid ${theme.colors.secondary};
   font-size: 1rem;
   font-weight: bold;
