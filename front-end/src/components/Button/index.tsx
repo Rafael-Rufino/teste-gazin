cd18ebaed55edc0d8ab1cc8956de210bbafbd931
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 20:23:03 -0300

    refactor: ajuste no component de butao

--- front-end/src/components/Button/index.tsx
+++ front-end/src/components/Button/index.tsx
@@ -2,7 +2,13 @@ import { IProps } from "./interface";
 
 import { Container } from "./styles";
 
-export const Button = ({ children, type, onClick, ...rest }: IProps) => {
+export const Button = ({
+  children,
+
+  type,
+  onClick,
+  ...rest
+}: IProps) => {
   return (
     <Container onClick={onClick} {...rest}>
       {children}
--- front-end/src/components/Button/interface.ts
+++ front-end/src/components/Button/interface.ts
@@ -2,4 +2,5 @@ export interface IProps {
   onClick?: () => void;
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
+  style?: React.CSSProperties;
 }
