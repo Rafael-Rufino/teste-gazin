8a7a3f7ab09e8b4c06583984f4ff32598711c9f7
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 14:36:48 -0300

    feat: developer

--- front-end/src/components/Developer/index.jsx
+++ front-end/src/components/Developer/index.jsx
@@ -1,5 +1,35 @@
-import { Container } from "./styles";
+import { useContext } from "react";
 
-export function Developer({}) {
-  return <Container></Container>;
+import { Container, ButtonArea, Button } from "./styles";
+import { IoTrashBinSharp, IoPencil } from "react-icons/io5";
+
+import { AuthContext } from "../../contexts/DeveloperContext";
+
+export function Developer({ id, name, gender, birthDate, age, hobby, level }) {
+  const { handleEdit, handleDelete } = useContext(AuthContext);
+
+  return (
+    <li>
+      <Container>
+        <h2>{name}</h2>
+        <h2>{gender}</h2>
+        <h2>{birthDate}</h2>
+        <h2>{age}</h2>
+        <h2>{hobby}</h2>
+        <h2>{level}</h2>
+        <ButtonArea>
+          <Button
+            onClick={() =>
+              handleEdit(id, name, gender, birthDate, age, hobby, level)
+            }
+          >
+            <IoPencil />
+          </Button>
+          <Button onClick={() => handleDelete(id)}>
+            <IoTrashBinSharp />
+          </Button>
+        </ButtonArea>
+      </Container>
+    </li>
+  );
 }
