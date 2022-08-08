e2167385e803e450607ec866e2870031f970c019
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 12:32:13 -0300

    refator: ajuste no header

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,17 +1,14 @@
-import { useContext } from "react";
-import { VideoContext } from "../../contexts/VideoContext";
 import { Button } from "../Button";
 
 import { Container } from "./styles";
 
 export default function Header() {
-  const { handleAdd } = useContext(VideoContext);
   return (
     <Container>
       <h2>
         Developer <span>Registration</span>
       </h2>
-      <Button onClick={handleAdd}>Adicionar Desenvolvedor</Button>
+      <Button onClick={() => {}}>Adicionar Desenvolvedor</Button>
     </Container>
   );
 }
