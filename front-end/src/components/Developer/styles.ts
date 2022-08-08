31e8c74fd4fab3451d82a87bf30df387b14994a4
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 20:23:41 -0300

    style: ajuste no componente de developer

--- front-end/src/components/Developer/styles.ts
+++ front-end/src/components/Developer/styles.ts
@@ -1,52 +1,19 @@
 import styled from "styled-components";
+
 import theme from "../../styles/themes";
 
-export const Container = styled.div`
-  background: ${theme.colors.heading};
-  padding: 10px;
-  height: 200px;
-  width: 250px;
-  border-radius: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.08);
+export const Container = styled.tr`
+  td {
+    padding: 1rem 2rem;
+    border: 0;
+    color: ${theme.colors.heading};
+    background: ${theme.colors.black};
+  }
+`;
+
+export const Wrraper = styled.div`
   display: flex;
-  justify-content: space-evenly;
-  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-
-  h2 {
-    color: ${theme.colors.black};
-    font-size: 1rem;
-    text-align: center;
-  }
-  a {
-    border-left: 3px solid ${theme.colors.text};
-    padding-left: 10px;
-    max-width: 230px;
-    color: ${theme.colors.text};
-    font-style: italic;
-    align-self: start;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    transition: opacity 0.2s ease-out;
-
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-export const ButtonArea = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-`;
-export const Button = styled.button`
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-  transition: opacity 0.2s ease-out;
-
-  &:hover {
-    opacity: 0.8;
-  }
+  gap: 10px;
 `;
