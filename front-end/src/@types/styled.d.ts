997d4929260a1d134652f2892a5116c6ae8cfad5
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-08 01:57:50 -0300

    feat: criando a tipagem para o estilo

---
+++ front-end/src/@types/styled.d.ts
@@ -0,0 +1,7 @@
+import "styled-components";
+import theme from "../styles/themes";
+
+declare module "styled-components" {
+  type ThemeType = typeof theme;
+  export interface DefaultTheme extends ThemeType {}
+}
